d474150b06034e7ca6fdd4293410fca956b78b01
Yiseth Gutiérrez <yiseth.gutierrez@bblabs.io>
2026-02-05 10:41:00 -0500

    Arreglo de DATABASE_URL

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,7 @@ import pytz
 
 
 # Database setup
-SQLALCHEMY_DATABASE_URL = "sqlite:///./ahorro.db"
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./ahorro.db")
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
